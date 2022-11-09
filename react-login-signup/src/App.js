2eff264a98ccb3d93a644c2bbc7ad512d8156c78
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-11-09 17:14:00 +0300

    added login page

--- react-login-signup/src/App.js
+++ react-login-signup/src/App.js
@@ -2,8 +2,8 @@ import React from 'react'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import Login from './components/login.component'
-import SignUp from './components/signup.component'
+import Login from './components/login'
+import SignUp from './components/signup'
 function App() {
   return (
     <Router>
