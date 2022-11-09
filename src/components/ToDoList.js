3dd4590b13c7f16abc5cab0a78efdaaa16872c7c
isir10 <isir.malak@moringaschool.com>
2022-11-09 16:41:01 +0300

    TD-6 added modals folder createtask file and incorporated reactstrap into my file

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -4,8 +4,8 @@ const ToDoList =() => {
   return (
     <>
      <div className='header text-center'>
-        <h3>Todo List</h3>
-        <button className='btn btn-primary'>Create Task</button>
+        <h3>Notes App</h3>
+        <button className="btn btn-primary mt-2">Create Task</button>
 
     </div>
     <div className='task-container'>
---
+++ src/modals/createTask.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+
+const createTask() => {
+  return (
+    <div>
+    </div>
+  );
+};
+
+export default createTask 
