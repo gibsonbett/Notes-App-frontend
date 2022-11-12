15491e2760ce4b368dca35a790bf05825e6ecbc3
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-11-12 21:54:24 +0300

    styled signup container

---
+++ src/components/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+// import ToDoList from './components/ToDoList';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SignUp from './components/Signup';
+function App() {
+  return (
+    <div className="App">
+      {/* <ToDoList /> */}
+      
+        <Routes>
+          <Route path='/' element = { <SignUp />}/>
+          {/* <Route exact path='/' element = { <Search />}/>
+          <Route exact path='/' element = { <ToDoList />}/> */}
+        </Routes>
+       
+    </div>
+
+  );
+     
+    }
+     
+
+
+export default App;
