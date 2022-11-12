b5ea34c7af3d081ab4f4cc05805ba1d4cb870da9
gibsonbett <gibson.bett@student.moringaschool.com>
2022-11-13 01:25:21 +0300

    write a function navbar,  add component to navlinks

---
+++ src/components/NavBar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+function NavBar() {
+    return (
+      <div className="navbar">
+        {/* <NavLink id="nav_items" to="/">
+          Home
+        </NavLink> */}
+        <NavLink id="nav_items" to="/login">
+        <button className="btn btn-primary mt-2">Login</button>
+          </NavLink>
+        <NavLink id="nav_items" to="/todolist">
+        <button className="btn btn-primary mt-2">Notes</button>
+        </NavLink>
+        <NavLink id="nav_items" to="/signup">
+        <button className="btn btn-primary mt-2">Sign Up</button>
+        </NavLink>
+      </div>
+    );
+  }
+  
+  export default NavBar;
