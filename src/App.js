c45d1ba7dc74938acfb4485b33e6d21e22ccfdfa
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-11-13 10:02:13 +0300

    import signup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import ToDoList from './components/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css'
-
+import Signup from './components/signup';
 function App() {
   return (
     <div className="App">
