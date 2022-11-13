a49ba99d6c87c89c104910da72dfe74f78b46eec
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-11-13 12:06:40 +0300

    routing

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 // import ToDoList from './components/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Signup from './components/Signup';
+import NavBar from './components/Nav';
+import Login from './components/Login';
 function App() {
   return (
     <div className="App">
       {/* <ToDoList /> */}
       <Routes>
           <Route path='/' element = { <Signup />}/>
+          <Route path='/' element = {<NavBar/>}/>
+          <Route path='/' element= {<Login/>}/>
         </Routes>
     </div>
   );
