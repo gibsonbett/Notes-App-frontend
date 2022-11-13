7b08591ffa39d9e36d7e98e721a02d6daed17a40
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-11-13 15:44:28 +0300

    finished styling signup

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import "../components/login.css";
+import "../components/signup.css";
 
 function Signup({ addNewUser }) {
     const initFormState = {
@@ -43,17 +43,32 @@ function Signup({ addNewUser }) {
     }
 
     return (
-        <div>
+        <div class = "signup-box">
+            <h2><b>Signup</b></h2>
+            <br></br>
+            <h3><b>Welcome to Notes App</b></h3>
+            <br></br>
+            <h4> <i>Signup to save all Notes and access them from anywhere </i></h4>
+            <br></br>
             <form  className = 'formWrapper' onSubmit = {handleSubmit} autoComplete="off">
+            <div class = "user-box">
                 <label className = 'label' htmlFor = 'username'></label>
                 <input className = 'input' id = 'username' type = 'text' name = 'username' placeholder = 'Username' value = {formState.username} onChange = {formChange} required />
+            </div>
+            <br></br>
+            <div class = "user-box">  
                 <label className = 'label' htmlFor = 'email'></label>
                 <input className = 'input' id = 'email' type = 'email' name = 'email' placeholder = 'Email address' value = {formState.email} onChange = {formChange} required />
+            </div>
+            <br></br>
+            <div class = "user-box">
                 <label className = 'label' htmlFor = 'password'></label>
                 <input className = 'input' id = 'password' type = 'password' name = 'password' placeholder = 'Password' value = {formState.password} onChange = {formChange} required />
                 <label className = 'label' htmlFor = 'passwordConfirm'></label>
                 <input className = 'input' id = 'passwordConfirm' type = 'password' name = 'passwordConfirm' placeholder = 'Confirm password' value = {passConfirmation.passwordConfirm} onChange = {passwordConfChange} required />
-                <button className = 'formBtn' type = 'submit'>Create Account</button>
+                <div class = "button-form"></div>
+                <button className = 'formBtn' type = 'submit'>SUBMIT</button>
+            </div>
             </form>
         </div>
     )
