740cee6102ca40b2ac53eec467725a4ab5813944
Cynthia-Ontiri <cynthia.otiri@students.moringaschool.com>
2022-11-13 10:18:55 +0300

    added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
-import ToDoList from './components/ToDoList';
+import { Routes, Route } from 'react-router-dom';
+// import ToDoList from './components/ToDoList';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import Signup from './components/signup';
+import Signup from './components/Signup';
 function App() {
   return (
     <div className="App">
-      <ToDoList />
-     
+      {/* <ToDoList /> */}
+      <Routes>
+          <Route path='/' element = { <Signup />}/>
+        </Routes>
     </div>
   );
 }
